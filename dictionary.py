5deeadc3e615aaa29cfdd7d60189bccb36207869
iamonk <anurag.rawal97@gmail.com>
2021-03-07 23:15:12 +0530

    Downloading And playing sound

--- dictionary.py
+++ dictionary.py
@@ -3,11 +3,13 @@ import secret
 import sub_direc_rule as sb
 import requests as requests
 import time
+import vlc 
+from playsound import playsound
 
 
 
 #word=input("Enter Word to be searched:  ")
-word="3-D"
+word="prejudice"
 
 
 #Final url
@@ -29,9 +31,19 @@ pron_list=r[0]['hwi']['prs']
 sound=[x['sound']['audio'] for x in pron_list]
 
 sub_dir_l=sb.sub_dir(sound)
-print(sub_dir_l)
+# print(sub_dir_l)
 
 audio_url=f"{secret.audio_url}{sub_dir_l[0]}/{sound[0]}.mp3"
+
+with requests.get(audio_url) as rq:
+    with open('audio.mp3','wb') as file:
+        file.write(rq.content)
+
+playsound('audio.mp3')
+
+# p=vlc.MediaPlayer(audio_url)
+# p.play()
+
 
 
 # audio_url=f"{secret.audio_url}" 
